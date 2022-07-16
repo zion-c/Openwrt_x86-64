5429052e01fb458ae06fbf47b5dd0bbddc0a9037
zion-c <42664548+zion-c@users.noreply.github.com>
2022-07-17 02:41:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,6 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
